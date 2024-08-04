d2c5b208c4a4af82c147a403d4b106465d0fa2e2
Manjunath <manjunathdtc3@gmail.com>
2024-08-05 01:03:59 +0530

    Sum of given integer

---
+++ src/SumOfDigit.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        int sum = 0;
+        int lastDigit;
+        int n=sc.nextInt();
+        int temp=n;
+        while (n>0)
+        {
+            lastDigit=n%10;
+            sum +=lastDigit;
+            n /=10;
+        }
+        System.out.println("Sum of "+temp+" is: "+sum);
+    }
+}
