c37e280b07e1cb89c8c8bcabd0d614cc5f1af904
Manjunath <manjunathdtc3@gmail.com>
2024-08-05 01:19:21 +0530

    Counting number of digits in given number

---
+++ src/DigitCount.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DigitCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        int n=sc.nextInt();
+        int temp = n;
+        int count = 0;
+        while (n>0)
+        {
+            n /= 10;
+            count++;
+        }
+        System.out.println("The number of digits in "+temp+" is: "+count);
+    }
+}
