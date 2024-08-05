83864c3fbfe66ff8536f17f3618e4588296e2e5b
Manjunath <manjunathdtc3@gmail.com>
2024-08-05 11:52:42 +0530

    Program to find whether the given number is Palindrome Number or Not

---
+++ src/PalindromeNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        int n=sc.nextInt();
+        int rev=0;
+        int lastDigit ,temp=n;
+        while (n>0)
+        {
+            lastDigit = n % 10;
+            rev = rev*10 + lastDigit;
+            n /=10;
+        }
+        if (temp == rev)
+        {
+            System.out.println("The given number " + temp + " is a Palindrome Number");
+        }
+        else
+        {
+            System.out.println("The given number " + temp + " is Not a Palindrome Number because "+temp+" != "+rev);
+        }
+    }
+}
