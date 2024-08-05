c8693769350639f9af7da14c7af208857afb4eac
Manjunath <manjunathdtc3@gmail.com>
2024-08-05 12:08:37 +0530

    Program to reverse a given number

---
+++ src/ReverseNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        int n=sc.nextInt();
+        int rev=0;
+        int lastDigit ,temp=n;
+        while (n>0)
+        {
+            lastDigit = n % 10;
+            rev = rev*10 + lastDigit;
+            n /=10;
+        }
+        System.out.println("The reverse of number "+temp+" is: "+rev);
+    }
+}
