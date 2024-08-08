7f4fa7aaceeaa0ff80ed2339e4a1796de5ed4179
Manjunath <manjunathdtc3@gmail.com>
2024-08-08 10:19:48 +0530

    program to find the Digit Frequency of given number completed

--- src/DigitFrequency.java
+++ src/DigitFrequency.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class DigitFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input: ");
+        int n=sc.nextInt();
+        sc.close();
+        int lastdigit;
+        int[] digit = new int[10];
+        while (n>0){
+            lastdigit=n%10;
+            for (int i = 0; i < 10; i++) {
+                if(i==lastdigit)
+                    digit[i]++;
+            }
+            n=n/10;
+        }
+        for (int i = 0; i < 10; i++) {
+            if(digit[i]!=0)
+                System.out.println("Frequency of "+i+" is: "+digit[i]);
+        }
+    }
 }
