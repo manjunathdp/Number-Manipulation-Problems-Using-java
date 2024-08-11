dda5592783ed94cf07ba39ded46133e283342a0a
Manjunath <manjunathdtc3@gmail.com>
2024-08-11 11:42:32 +0530

    program to convert Decimal To Binary

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String binary="";
+        while(n!=0)
+        {
+            int rem=n%2;
+            binary = rem + binary;
+            n /= 2;
+        }
+        System.out.print(binary);
+    }
+}
